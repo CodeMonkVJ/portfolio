9da055625b25a59e5b9a7beaeef6809d56552150
shaurya <shaurya.vijayvargiya@gmail.com>
2024-04-15 18:40:02 +0530

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const urls = [
     "https://utfs.io/f/76c34a94-d8db-40fc-aaa4-2a5e322b7ede-bz2sor.jpg"
 ];
@@ -7,13 +9,17 @@ const images = urls.map((url, index) => ({
     url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+    const posts = await db.query.posts.findMany();
     return (
         <main className="">
             <div className="flex flex-wrap gap-4">
-                {[...images, ...images, ...images, ...images].map((image) => {
+                {posts.map((post) => (
+                    <div key={post.id}>{post.name}</div>
+                ))}
+                {[...images, ...images, ...images, ...images].map((image, index) => {
                     return (
-                        <div key={image.id} className="w-36">
+                        <div key={image.id + "-" + index} className="w-36">
                             <img src={image.url} />
                         </div>
                     );
