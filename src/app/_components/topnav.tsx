309aeede8af51d90b4ad21736f54ddf6cb90cc0a
shaurya <shaurya.vijayvargiya@gmail.com>
2024-04-18 23:37:38 +0530

    image upload working

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,15 +1,18 @@
+"use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
     return (
         <nav className="flex items-center justify-between w-full p-4 text-xl 
             font-semibold border-b">
             <div>Portfolio</div>
-            <div>
+            <div className="flex flex-row">
                 <SignedOut>
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
+                    <UploadButton endpoint="imageUploader"/>
                     <UserButton />
                 </SignedIn>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
     const images = await db.query.images.findMany({
-        orderBy: (model, {desc}) => desc(model.id),
+        orderBy: (model, { desc }) => desc(model.id),
     });
     return (
         <main className="">
-            <div className="flex flex-wrap gap-4">
-                {[...images, ...images, ...images, ...images].map((image, index) => {
-                    return (
-                        <div key={image.id + "-" + index} className="w-36 flex flex-col">
-                            <img src={image.url} />
-                            <div>{image.name}</div>
-                        </div>
-                    );
-                })}
-            </div>
+            <SignedOut>
+                <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+            </SignedOut>
+            <SignedIn>
+                <div className="flex flex-wrap gap-4">
+                    {images.map((image) => {
+                        return (
+                            <div key={image.id} className="w-36 flex flex-col">
+                                <img src={image.url} />
+                                <div>{image.name}</div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </SignedIn>
         </main>
     );
 }
+
