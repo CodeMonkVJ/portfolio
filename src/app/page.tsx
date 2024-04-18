3b75cbf518ab769dce0255bdad1cc93e066eec37
shaurya <shaurya.vijayvargiya@gmail.com>
2024-04-18 22:10:44 +0530

    added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,18 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const urls = [
-    "https://utfs.io/f/76c34a94-d8db-40fc-aaa4-2a5e322b7ede-bz2sor.jpg"
-];
-
-const images = urls.map((url, index) => ({
-    id: index + 1,
-    url,
-}));
-
 export default async function HomePage() {
-    const posts = await db.query.posts.findMany();
+    const images = await db.query.images.findMany({
+        orderBy: (model, {desc}) => desc(model.id),
+    });
     return (
         <main className="">
             <div className="flex flex-wrap gap-4">
-                {posts.map((post) => (
-                    <div key={post.id}>{post.name}</div>
-                ))}
                 {[...images, ...images, ...images, ...images].map((image, index) => {
                     return (
-                        <div key={image.id + "-" + index} className="w-36">
+                        <div key={image.id + "-" + index} className="w-36 flex flex-col">
                             <img src={image.url} />
+                            <div>{image.name}</div>
                         </div>
                     );
                 })}
