8f04a0b8f3f2366491c200028f4f0722a3ae72de
shaurya <shaurya.vijayvargiya@gmail.com>
2024-04-19 09:55:59 +0530

    multiple image upload

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+    const router = useRouter();
     return (
         <nav className="flex items-center justify-between w-full p-4 text-xl 
             font-semibold border-b">
@@ -12,7 +14,12 @@ export function TopNav() {
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
-                    <UploadButton endpoint="imageUploader"/>
+                    <UploadButton 
+                    endpoint="imageUploader" 
+                    onClientUploadComplete={() => {
+                        router.refresh();
+                    }} 
+                    />
                     <UserButton />
                 </SignedIn>
             </div>
