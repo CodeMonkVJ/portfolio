7b096cfe84d5c2d19444ce3199af5e83f83fcba4
shaurya <shaurya.vijayvargiya@gmail.com>
2024-04-20 01:27:23 +0530

    fixed parallel routes images and full page views

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,8 +1,27 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { photoId: number }) {
     const image = await getImage(props.photoId);
+    const uploaderInfo = await clerkClient.users.getUser(image.userId);
+    console.log(uploaderInfo);
     return (
-        <img src={image.url} className="w-96" />
+        <div className="flex h-full w-full min-w-0">
+            <div className="flex-shrink flex justify-center items-center">
+                <img src={image.url} className="flex-shrink object-contain" />
+            </div>
+
+            <div className="flex w-48 flex-shrink-0 flex-col border-l">
+                <div className="text-lg text-center border-b p-2">{image.name}</div>
+                <div className="flex flex-col p-2">
+                    <span>Uploaded By:</span>
+                    <span>{uploaderInfo.username}</span>
+                </div>
+                <div className="flex flex-col p-2">
+                    <span>Created On:</span>
+                    <span>{new Date(uploaderInfo.createdAt).toLocaleDateString()}</span>
+                </div>
+            </div>
+        </div>
     );
 }
